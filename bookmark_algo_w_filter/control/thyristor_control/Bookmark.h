87f53350843d6cc6beb6a5c5728a663978b3ba8a
MRadekTCZ <97914167+MRadekTCZ@users.noreply.github.com>
2023-11-05 19:06:08 +0100

    Update Bookmark.h

--- bookmark_algo_w_filter/control/thyristor_control/Bookmark.h
+++ bookmark_algo_w_filter/control/thyristor_control/Bookmark.h
@@ -1,49 +1,43 @@
 #pragma once
 
-#define SAMPLES_10ms 3020 //liczba próbek do zliczania watchdoga
-#define MARGIN 2.0 //margines k¹ta do prze³¹czania
-#define NO_LOAD_STATE 0.2 //Próg wartoœci pr¹du RMS, od której uznawany jest brak obci¹zenia
+#define SAMPLES_10ms 3020 // Number of samples for watchdog counting
+#define MARGIN 2.0 // Angle margin for switching
+#define NO_LOAD_STATE 0.2 // Threshold value of RMS current below which the load is considered absent
 #define NUMBER_OF_TAPS 5
-//Stany tyrystorów
-
-
-
+// Thyristor states
 
 /*******************************/
-//Definicja obiektu PPZ
+// Definition of the TPPZ object
 enum ON_OFF { OFF = 0, ON = 1 };
 typedef enum ON_OFF Thyri_Gate_State;
-struct Tap_Thyristor_State //Struktura bitowa - stan tyrystora dolnego i gornego
+struct Tap_Thyristor_State // Bit structure - state of upper and lower thyristors
 {
-	Thyri_Gate_State Tap_up;
-	Thyri_Gate_State Tap_down;
+    Thyri_Gate_State Tap_up;
+    Thyri_Gate_State Tap_down;
 };
 enum PHASE { L1 = 1, L2 = 2, L3 = 3 };
-struct Tap_State //Struktura przelacznika zaczepow
+struct Tap_State // Switch object structure
 {
-	struct Tap_Thyristor_State Tap_select[NUMBER_OF_TAPS]; //Dla ka¿dego zaczepu struktura bitowa z dwoma tyrystorami
-	enum PHASE phase; //informacja o wybranej fazie przelacznika
+    struct Tap_Thyristor_State Tap_select[NUMBER_OF_TAPS]; // For each tap, a bit structure with two thyristors
+    enum PHASE phase; // Information about the selected switch phase
 };
 typedef struct Tap_State TPPZ;
 TPPZ tppz[3];
 /************************************/
 
+enum change_types { step, jump }; // Tap change mode - step or jump (one by one)
+enum Load { Resistive, Inductive, Capacitive }; // Types of loads
 
-enum change_types { step, jump }; //Tryb zmiany zaczepów - skokowa lub schodkowa (po kolei, po jednym)
-
-enum Load { Resistive, Inductive, Capacitive }; //Typy obci¹¿eñ
-
-
-struct Watchdog {   // Struktura Watchdoga
-	long int counter;
-	int error_code;
-	int cnt_reset_enable;
-	short int last_state_chage;
+struct Watchdog {   // Watchdog structure
+    long int counter;
+    int error_code;
+    int cnt_reset_enable;
+    short int last_state_chage;
 };
 enum Load LOAD_DET(float thetaU, float thetaI);
 short int Tap_diff(short int actual_tap_set, short int actual_tap, enum change_types change_type);
 
-//Finalnie stwierdzi³em, ¿e algorytm g³ówny lepiej, aby nie by³ funkcj¹. £atwiej podgl¹daæ zmienne itp
-//short int BOOKMARK(TPPZ* tppz, short int actual_tap_set_global, double voltage_phase, double current_phase, double actual_current, double q_error, struct Watchdog* watchdog);
+// Ultimately, I decided that the main algorithm is better not to be a function. Easier to monitor variables, etc.
+// short int BOOKMARK(TPPZ* tppz, short int actual_tap_set_global, double voltage_phase, double current_phase, double actual_current, double q_error, struct Watchdog* watchdog);
 void program_Watchdog(TPPZ* tppz, struct Watchdog* watchdog, short int* state_machine, short int* actual_tap, short int actual_tap_set);
 int Tap_bit(TPPZ tppz, int tap, int enable);
